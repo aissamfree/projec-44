00abc4edc5ed0668eca164f7f32183b3e24377a6
HP 840 G6 <HP 840 G6@DESKTOP-IGP3C7T>
2025-09-03 14:03:01 +0100

    Ajoutez des fichiers projet.

---
+++ project 44 #44.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short int  day;
+    cout << "please enter day :" << endl;
+    cin >> day;
+
+
+    if (day = 1)
+    {
+        
+        cout << "it is sunday"  << endl;
+    }
+    else if (day = 2)
+    {
+
+        cout << "it is monday" << endl;
+    }
+    else if (day = 3)
+    {
+
+        cout << "it is tuesday" << endl;
+    }
+    else if (day = 4)
+    {
+
+        cout << "it is wednesday" << endl;
+    }
+    else if (day = 5)
+    {
+
+        cout << "it is monday" << endl;
+    }
+    else if (day = 6)
+    {
+
+        cout << "it is friday" << endl;
+    }
+    else if (day = 7)
+    {
+
+        cout << "it is saturday" << endl;
+    }
+    else 
+    {
+
+        cout << "wrong day" << endl;
+    }
+
+    return 0;
+}
